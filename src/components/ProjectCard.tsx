93c608184456a2908ed13a456cd156478936de16
Qiang Loozen <qiangloozen@gmail.com>
2023-07-22 21:29:09 +0200

    created simple modal

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,37 +1,53 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Project } from "../models/ProjectModel";
-import { motion } from "framer-motion";
-import { HoverScaleVariant } from "../Animations";
+import { AnimatePresence, motion } from "framer-motion";
+import Modal from "./Modal";
 
 type Props = {
   project: Project;
 };
 
 const ProjectCard = ({ project }: Props) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const close = () => {
+    setModalOpen(false);
+    document.body.style.overflow = "unset";
+  };
+  const open = () => {
+    setModalOpen(true);
+    document.body.style.overflow = "hidden";
+  };
+
   return (
-    <motion.div
-      className="project-card-container"
-      initial={{ scale: 1 }}
-      whileHover={{
-        scale: 1.025,
-      }}
-      transition={{
-        duration: 0.25,
-      }}
-    >
-      <img src={project.thumbnail} alt="" />
-      <div className="project-card-content">
-        <h4>{project.name}</h4>
-        <p>{project.description}</p>
-        <div className="tag-container">
-          {project.tags.map((tag) => (
-            <span className="tag" key={tag}>
-              {tag}
-            </span>
-          ))}
+    <div>
+      <motion.div
+        className="project-card-container"
+        initial={{ scale: 1 }}
+        whileHover={{
+          scale: 1.025,
+        }}
+        transition={{
+          duration: 0.25,
+        }}
+        onClick={() => (modalOpen ? close() : open())}
+      >
+        <img src={project.thumbnail} alt="" draggable="false" />
+        <div className="project-card-content">
+          <h4>{project.name}</h4>
+          <p>{project.description}</p>
+          <div className="tag-container">
+            {project.tags.map((tag) => (
+              <span className="tag" key={tag}>
+                {tag}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+      <AnimatePresence initial={false} mode="wait">
+        {modalOpen && <Modal handleClose={close} project={project} />}
+      </AnimatePresence>
+    </div>
   );
 };
 
