1f2419be1b417851a10cef8f3d17028eea08770c
Qiang Loozen <qiangloozen@gmail.com>
2024-06-22 00:21:27 +0200

    added strapi content types

---
+++ portfolio-cms/src/api/development-category/controllers/development-category.ts
@@ -0,0 +1,7 @@
+/**
+ * development-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::development-category.development-category');
